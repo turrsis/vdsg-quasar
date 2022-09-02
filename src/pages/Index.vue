07cd63ab67576e1aad5cbff7db65296376f398fe
turrsis <turrsis@gmail.com>
2022-09-02 10:14:00 +0300

    fix

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,24 +1,16 @@
 <template>
-  <q-page class="flex flex-center" v-designer.true>
-    <q-btn
-      color="white"
-      text-color="black"
-      label="Standard"
-      to="/about"
-    />
-    <q-img
-      src="~assets/quasar-logo-vertical.svg"
-      spinner-color="blue"
-      width="200px"
-      height="200px"
-    />
-  </q-page>
+    <q-page class="flex flex-center" v-designer.true>
+        <div>
+            <div>
+                <q-btn color="white" text-color="black" label="Standarde" to="/about" :round="false"/>
+            </div>
+        </div>
+        <q-img src="~assets/quasar-logo-vertical.svg" spinner-color="blue" width="200px" height="200px"/>
+    </q-page>
 </template>
-
 <script>
-import { defineComponent } from 'vue'
-
+import { defineComponent } from 'vue';
 export default defineComponent({
-  name: 'PageIndex'
-})
+    name: 'PageIndex'
+});
 </script>
