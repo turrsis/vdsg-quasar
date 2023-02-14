ef1d47dc70538bf7393d384fe09b012094e98cb1
turrsis <turrsis@gmail.com>
2023-02-14 09:46:47 +0200

    !!! fix:: nuxt - add page

--- .designer/config.mjs
+++ .designer/config.mjs
@@ -1,10 +1,14 @@
 export default {
     key: 'vdsg-tests-quasar',
     label: 'quasar tests',
-    tests: {
-        uiSpecsPath: '.designer-tests/.spec',
+    app: {
+        tests: {
+            uiSpecsPath: '.designer-tests/.spec',
+        },
+        router: {
+            entryPoint: 'src/router/index.js',
+        },
     },
-    routesEntryPoint: 'src/router/index.js',
     originhost: 'http://localhost:8080',
     server: {
         host: 'localhost',
