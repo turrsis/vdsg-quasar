3c900fe94490d157b54378caa9af436ab12642a9
turrsis <turrsis@gmail.com>
2023-04-05 14:12:53 +0300

    refactor:: MetadataConfig

--- .designer/metadata.templates.mjs
+++ .designer/metadata.templates.mjs
@@ -1,26 +1,16 @@
-export default {
-    groups: {
-        'quasar-elements': 'quasar-elements',
-    },
-    elements: {
-        'quasar-elements': {
-            label: 'quasar elements',
-            templates: {
-                'div': {
-                    label: 'div-text',
-                    template: {
-                        tag: 'div',
-                        children: ['quasar text in div']
-                    }
-                },
-                'span': {
-                    label: 'span-text',
-                    template: {
-                        tag: 'span',
-                        children: ['quasar text in span']
-                    }
-                }
-            }
-        },
-    }
-}
+export default [{
+    key: 'quasar-elements',
+    children: [{
+        key: 'div-text',
+        src: { template: {
+            tag: 'div',
+            children: ['quasar text in div']
+        } }
+    }, {
+        key: 'span-text',
+        src: { template: {
+            tag: 'span',
+            children: ['quasar text in span']
+        } }
+    }]
+}]
